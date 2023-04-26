6c6e485ba3a2a8a6fbc20660c4a0125c3cd58d49
Danni4421 <85418881+Danni4421@users.noreply.github.com>
2023-04-26 18:19:39 +0700

    feature : finish all feature

--- src/script/script.js
+++ src/script/script.js
@@ -2,10 +2,14 @@ const valueInput = document.getElementById('input-todo')
 const RENDER_TODOS = 'render-todos'
 const RENDER_IN_PROGRESS = 'render-in-progress'
 const RENDER_DONE = 'render-done'
+const REMOVE_TODOS = 'remove-todos'
+const REMOVE_IN_PROGRESS = 'remove-inprogress'
 const todos = []
 const onProgess = []
 const isDone = []
 let tempTodos
+let tempOnProgress
+let tempDone
 
 window.addEventListener('DOMContentLoaded', () => {
     const submitForm = document.getElementById('todo-input')
@@ -26,52 +30,92 @@ const addTodo = () => {
     }
     todos.push(result)
     tempTodos = result
-    valueInput.value = ""
+    valueInput.value = ''
+    
+    saveTodos()
     document.dispatchEvent(new Event(RENDER_TODOS))
 }
 
+const todoTemp = document.getElementById('todos')
+const onProgressTemp = document.getElementById('on-progress')
 document.addEventListener(RENDER_TODOS, () => {
-    const todoTemp = document.getElementById('todos')
-    todoTemp.append(createCard(tempTodos))    
+    todoTemp.innerHTML = ""
+    for (const todo of todos) {
+        todoTemp.append(createCard(todo.id, todo.value, 0))
+    }
 })
 
 document.addEventListener(RENDER_IN_PROGRESS, () => {
-
+    onProgressTemp.innerHTML = ""
+    for (const prog of onProgess) {
+        onProgressTemp.append(createCard(prog.id, prog.value, 1))
+    }
 })
 
 document.addEventListener(RENDER_DONE, () => {
-
+    const isCompleted = document.getElementById('isDone')
+    isCompleted.innerHTML = ""
+    for (const done of isDone) {
+        isCompleted.append(createCard(done.id, done.value, 2))
+    }
 })
 
-const createCard = ({id, value}) => {
+const createCard = (id, value, different) => {
     const container = document.createElement('div')
     container.classList.add('sec')
-    container.classList.add('todo')
+    container.setAttribute('id', id)
     const h4 = document.createElement('h4')
     h4.innerText = value
-    const btnProcess = document.createElement('button')
-    btnProcess.classList.add('btn')
-    btnProcess.innerText = 'Process'
-    btnProcess.addEventListener('click', () => {
-        processTodo(id)
-    })
+    
+    if (different < 2) {
+        const btnProcess = document.createElement('button')
+        btnProcess.classList.add('btn')
+        switch (different) {
+            case 0:
+            container.classList.add('todo')
+            btnProcess.innerText = 'Process'
+            btnProcess.addEventListener('click', () => {
+                processTodo(id)
+            })
+            break;
+            case 1:
+            container.classList.add('on-progress')
+            btnProcess.innerText = 'Done'
+            btnProcess.addEventListener('click', () => {
+                endProgress(id)
+            })
+            break;
+        }
+        container.append(h4, btnProcess)
+    } else {
+        container.classList.add('done')
+        container.append(h4)
+    }
 
-    container.append(h4, btnProcess)
     return container
 }
 
 const processTodo = id => {
-    const container = document.createElement('div')
-    container.classList.add('todo')
-    const h4 = document.createElement('h4')
-    h4.innerText = value
-    const btnProcess = document.createElement('button')
-    btnProcess.innerText = 'Process'
-    btnProcess.addEventListener('click', () => {
-        endProgress(id)
-    })
+    tempOnProgress = todos.find(todo => todo.id === id)
+    onProgess.push(tempOnProgress)
+    todos.splice(id, 1)
+    removeItem(0, id)
+
+    todoTemp.removeChild(document.getElementById(`${id}`))
+    saveOnProgress()
 
     document.dispatchEvent(new Event(RENDER_IN_PROGRESS))
 }
 
+const endProgress = id => {
+    tempDone = onProgess.find(onProg => onProg.id === id)
+    tempDone.isCompleted = true
+    isDone.push(tempDone)
+    onProgess.splice(id, 1)
+    removeItem(1, id)
 
+    onProgressTemp.removeChild(document.getElementById(`${id}`))
+    saveIsDone()
+    
+    document.dispatchEvent(new Event(RENDER_DONE))
+}
--- src/script/session.js
+++ src/script/session.js
@@ -12,11 +12,85 @@ const STORAGE_KEY_IS_DONE = 'isDone'
 
 // Check Compatibility Browser Storage
 const checkCompatibility = () => {
-    if (typeof(Storage) === undefined) {
+    if (typeof(Storage) === 'undefined') {
         console.log('Maaf Browser tidak mendukung Storage')
         return false
     } else {
         return true
     }
 }
+
+const saveTodos = () => {
+    if (checkCompatibility()) {
+        const toStringTodos = JSON.stringify(todos)
+        sessionStorage.setItem(STORAGE_KEY_TODOS, toStringTodos)
+    }
+    console.log(todos)
+}
+
+const saveOnProgress = () => {
+    const toStringOnProgress = JSON.stringify(onProgess)
+    sessionStorage.setItem(STORAGE_KEY_ON_PROGRESS, toStringOnProgress)
+}
+
+const saveIsDone = () => {
+    const toStringIsDone = JSON.stringify(isDone)
+    sessionStorage.setItem(STORAGE_KEY_IS_DONE, toStringIsDone)
+}
+
+document.addEventListener(SAVE_TODO_DONE, () => {
+
+})
+
+const loadTodos = () => {
+
+    const parse = JSON.parse(sessionStorage.getItem(STORAGE_KEY_TODOS))
+
+    if (parse !== null) {
+        for (const res of parse) {
+            todos.push(res)
+        }
+    }
+
+    document.dispatchEvent(new Event(RENDER_TODOS))
+}
+
+const loadOnProgress = () => {
+    const parse = JSON.parse(sessionStorage.getItem(STORAGE_KEY_ON_PROGRESS))
+
+    if (parse !== null) {
+        for (const res of parse) {
+            onProgess.push(res)
+        }
+    }
+
+    document.dispatchEvent(new Event(RENDER_IN_PROGRESS))
+}
+
+const loadIsDone = () => {
+    const parse = JSON.parse(sessionStorage.getItem(STORAGE_KEY_IS_DONE))
+
+    if (parse !== null) {
+        for (const res of parse) {
+            isDone.push(res)
+        }
+    }
+
+    document.dispatchEvent(new Event(RENDER_DONE))
+}
+
+const removeItem = (diff, id) => {
+    switch (diff) {
+        case 0:
+            sessionStorage.removeItem(STORAGE_KEY_TODOS, id)
+            break
+        case 1:
+            sessionStorage.removeItem(STORAGE_KEY_ON_PROGRESS, id)
+            break
+    }
+}
+
+loadTodos()
+loadOnProgress()
+loadIsDone()
 
